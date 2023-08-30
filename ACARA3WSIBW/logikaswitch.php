f206f2e171826bb9a482a35908e5976c444dd790
SaiaFadil <fadillahwahyunugraha@gmail.com>
2023-08-29 20:22:09 -0700

    Create logikaswitch.php

---
+++ ACARA3WSIBW/logikaswitch.php
@@ -0,0 +1,27 @@
+<?php
+$a=100;
+switch ($a)
+{
+case 0 :
+ echo "Angka Nol";
+ break;
+case 1 :
+ echo "Angka Satu";
+ break;
+case 2 :
+ echo "Angka Dua";
+ break;
+case 3 :
+ echo "Angka Tiga";
+ break;
+case 4 :
+ echo "Angka Empat";
+ break;
+case 5 :
+ echo "Angka Lima";
+ break;
+default :
+ echo "Angka diluar jangkauan";
+ break;
+}
+?>
