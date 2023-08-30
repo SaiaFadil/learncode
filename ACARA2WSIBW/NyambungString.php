3895c4ed2b958b3c6ac579804a15faa3b7b2c2d3
SaiaFadil <fadillahwahyunugraha@gmail.com>
2023-08-29 20:22:36 -0700

    Create NyambungString.php

---
+++ ACARA2WSIBW/NyambungString.php
@@ -0,0 +1,15 @@
+<?php
+$a = "Hello ";
+$hasil = "{$a} World!";
+echo $hasil; // Hello World!
+echo "<br />";
+
+
+$a = "belajar ";
+$b = "PHP ";
+$c = "di Politeknik Negeri Jember";
+$hasil= "Saya sedang {$a}{$b}{$c}";
+
+
+echo $hasil; // Saya sedang belajar PHP di Politeknik Negeri Jember
+?>
