a746ad9611c9ab4e3a0daa24ec2689b876ccb266
SaiaFadil <fadillahwahyunugraha@gmail.com>
2023-08-29 20:22:21 -0700

    Create Nomor3.php

---
+++ ACARA2WSIBW/Tugas/Nomor3.php
@@ -0,0 +1,13 @@
+<?php
+//Fadillah Wahyu Nugraha
+
+$tugas1 = "90";
+$tugas2 = "80";
+
+$hasil_gabungan = $tugas1 . $tugas2;  
+$hasil_integer = (int)$hasil_gabungan;
+
+echo $hasil_integer; 
+echo "\n";
+echo $hasil_integer + 10; 
+?>
