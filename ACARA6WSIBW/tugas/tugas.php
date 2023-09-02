8764e896694683ee52dd318faaac553a865b21f0
Fadil <fadillahwahyunugraha@gmail.com>
2023-09-02 17:16:23 +0700

    Create tugas.php

---
+++ ACARA6WSIBW/tugas/tugas.php
@@ -0,0 +1,34 @@
+<?php
+// Matriks A
+$matrixA = [
+    [1, 1, 1],
+    [2, 2, 2],
+    [3, 3, 3]
+];
+
+// Matriks B
+$matrixB = [
+    [3, 3, 3],
+    [2, 2, 2],
+    [1, 1, 1]
+];
+
+// Inisialisasi matriks hasil penjumlahan
+$resultMatrix = [];
+
+// Melakukan penjumlahan matriks A dan B
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        $resultMatrix[$i][$j] = $matrixA[$i][$j] + $matrixB[$i][$j];
+    }
+}
+
+// Menampilkan matriks hasil penjumlahan
+echo "Hasil Penjumlahan Matriks A dan B:\n";
+for ($i = 0; $i < 3; $i++) {
+    for ($j = 0; $j < 3; $j++) {
+        echo $resultMatrix[$i][$j] . " ";
+    }
+    echo "\n";
+}
+?>
