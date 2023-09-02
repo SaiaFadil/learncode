eb46172797323136a671e1253c68fb4aa084356e
Fadil <fadillahwahyunugraha@gmail.com>
2023-09-02 17:15:55 +0700

    Create array-basic.php

---
+++ ACARA6WSIBW/array-basic.php
@@ -0,0 +1,11 @@
+<?php
+$punakawan = array("Semar","Gareng","Petruk","Bagong");
+echo $punakawan[0]; 
+echo "<br>";
+echo $punakawan[3]; 
+$punakawan[1]="Semar";
+$punakawan[2]="Gareng";
+$punakawan[3]="Petruk";
+$punakawan[4]="Bagong";
+echo $punakawan[3];
+?>
